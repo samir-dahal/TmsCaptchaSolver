c72748156bc2eaeffdecac5b4af5494a34a9a400
samir <samir.dahal@crystalsolution.com.np>
2021-12-17 14:05:44 +0545

    first commit

---
+++ content.js
@@ -0,0 +1,47 @@
+//Captcha Img Data
+const captchaEnter = document.querySelector("#captchaEnter");
+captchaEnter.value = "Loading...";
+var img = document.querySelector("img[alt='Captcha'"),
+  observer = new MutationObserver((changes) => {
+    changes.forEach(async (change) => {
+      if (change.attributeName.includes("src")) {
+        captchaEnter.value = "Loading...";
+        const captchaResult = await getImageText(
+          await convertToBase64(img.src)
+        );
+        console.clear();
+        console.log(captchaResult);
+        captchaEnter.value = captchaResult;
+        captchaEnter.dispatchEvent(new Event("input"));
+      }
+    });
+  });
+observer.observe(img, { attributes: true });
+
+async function convertToBase64(src) {
+  const res = await axios.get(img.src, {
+    responseType: "arraybuffer",
+  });
+  return imageEncode(res.data);
+}
+function imageEncode(arrayBuffer) {
+  let u8 = new Uint8Array(arrayBuffer);
+  let b64encoded = btoa(
+    [].reduce.call(
+      new Uint8Array(arrayBuffer),
+      function (p, c) {
+        return p + String.fromCharCode(c);
+      },
+      ""
+    )
+  );
+  let mimetype = "image/jpeg";
+  return b64encoded;
+}
+async function getImageText(base64img) {
+  const res = await axios.post(
+    "https://samir.crystalhost.com.np/api/captchas",
+    { base64Image: base64img }
+  );
+  return res.data.result;
+}
